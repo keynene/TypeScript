687c0d4c53a005274eb13ffb5df9effd9cc364c2
keynene <nonung2@naver.com>
2023-07-23 12:00:59 +0000

    일반문법연습

--- typescript.js
+++ typescript.js
@@ -1,1 +1,15 @@
-"use strict";
+let 이름 :string = 'noi'
+let 나이 :number = 28
+let 출생지역 :string = 'Changwon'
+
+let favorite :{songTitle : string, singer : string} = {songTitle : '눈물이 더 가까운 사람', singer : '이홍기'}
+
+let project :{
+  member :string[],
+  days :number,
+  started :boolean,
+} = {
+  member : ['kim', 'park'],
+  days : 30,
+  started : true,
+}
