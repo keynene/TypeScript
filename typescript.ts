5f183e7a49d1bf61640146e66995e447467d2361
keynene <nonung2@naver.com>
2023-08-04 16:30:25 +0900

    interface 사용 : 확장용이 (같은이름 불가) : object 타입 지정 용이

--- typescript.ts
+++ typescript.ts
@@ -283,3 +283,54 @@
 
 
 // call('010-1111-2222', cutZero, removeDash)
+
+
+
+
+/** Object에 타입지정하려면 interface 이것도 있음 */
+// interface Student {
+//   name :string
+// }
+// interface Teacher extends Student { //확장
+//   age :number
+// }
+
+// let 학생 :Student = { name:'kim' }
+// let 선생 :Teacher = { name:'kim', age :20 }
+
+//숙제1
+interface Product {
+  brand :string,
+  serialNumber :number,
+  model :string[]
+}
+
+let 상품 :Product = { brand : 'Samsung', serialNumber : 1360, model : ['TV', 'phone'] }
+
+//숙제2
+interface Cart {
+  product :string,
+  price :number
+}
+
+let 장바구니 :Cart[] = [ { product : '청소기', price : 7000 }, { product : '삼다수', price : 800 } ]
+
+//숙제3
+interface NewCart extends Cart {
+  card :boolean
+}
+
+//숙제4
+interface MathObj {
+  plus : (x :number, y :number) => number,
+  minus : (x :number, y :number) => number
+}
+
+let oobj :MathObj = {
+  plus(x,y){
+    return x+y
+  },
+  minus(x,y){
+    return x-y
+  }
+}
